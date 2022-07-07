2f06a9e6f2a4e1f9c887fdfacfdf97c80cb827d0
Soomin Yim <soo15soo@kakao.com>
2022-07-07 22:35:38 +0900

    first commit

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from 'firebase/firestore'
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
-import Home from "./pages/Home";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyCJxQc-XqKHazk2KGCjOZHW1WtVBgnVMdk",
   authDomain: "test01-aba47.firebaseapp.com",
@@ -16,12 +15,11 @@ const firebaseConfig = {
   storageBucket: "test01-aba47.appspot.com",
   messagingSenderId: "28784931534",
   appId: "1:28784931534:web:b240d8e7fd5e4f94a2169f",
-  measurementId: "G-X0DXRMD433"
+  measurementId: "G-X0DXRMD433",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
